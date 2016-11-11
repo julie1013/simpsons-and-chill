5a2f7c12d05e6c972f65243eb426fedb44704cfc
julie1013 <julie1380@verizon.net>
2016-11-11 17:57:40 -0500

    Tweaked to make it so that you can't add to list twice...
    And that you can't see eps unless you're logged in

--- app/components/navbar-header/component.js
+++ app/components/navbar-header/component.js
@@ -3,4 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['navbar-header'],
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  auth: Ember.inject.service(),
 });
